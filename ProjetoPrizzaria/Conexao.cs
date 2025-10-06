c59a05ebd79910db89d1a90589c46a0b2022918a
2Bmodular <2bmod@bglab.local>
2025-10-06 20:10:39 -0300

    Conectando com o banco de dados

---
+++ ProjetoPrizzaria/Conexao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPrizzaria
+{
+    class Conexao
+    {
+        MySqlConnection con = new MySqlConnection("Data Source=localhost;Initial Catalog=BDPizzaria1;user=root;pwd=12345678");
+
+        public static string msg;
+
+        public MySqlConnection ConnectarBD()
+        {
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+
+        public MySqlConnection DesConnectarBD()
+        {
+            try
+            {
+                con.Close();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+    }
+}
